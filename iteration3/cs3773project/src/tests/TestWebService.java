8c108b589085a25bd705b63c1b938ed9b6397ddf
julianbutsa <jbruneault@gmail.com>
2014-11-24 12:51:48 -0600

    Did stuff

--- iteration3/cs3773project/src/tests/TestWebService.java
+++ iteration3/cs3773project/src/tests/TestWebService.java
@@ -11,12 +11,31 @@ import org.junit.Test;
 
 public class TestWebService {
 
+	
+	/*
+	 * 
+	 * Testign creating a WSStuff object
+	 */
 	@Test
-	public void testHTTP1(){
-		WSStuff simpleConnect = new WSStuff("http://devcloud.fulgentcorp.com/test.php");
-		String response  = simpleConnect.sendRequest(HTTPMethod.GET, "");
-		assertEquals("this is only a test", response);
-		
+	public void testWSStuff_null(){
+		String input  = null;
+		String iaexception_message = "WS URL must be valid HTTP or HTTPS web resource";
+		try{
+			WSStuff simpleConnect = new WSStuff(input);
+		}catch(IllegalArgumentException e){
+			assertEquals(iaexception_message, e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testWSStuff_emptystring(){
+		String input = "";
+		String iaexception_message = "WS URL must be valid HTTP or HTTPS web resource";
+		try{
+			WSStuff simpleConnect = new WSStuff(input);
+		}catch(IllegalArgumentException e){
+			assertEquals(iaexception_message, e.getMessage());
+		}
 	}
 	
 	@Test(expected =IllegalArgumentException.class)
@@ -27,13 +46,40 @@ public class TestWebService {
 
 	}
 	
-	@Test(expected = RuntimeException.class)
+	@Test
 	public void invalidurl(){
-		WSStuff simpleConnect = new WSStuff("http://xdevcloud.fulgentcorp.com/test.php");
-		String response  = simpleConnect.sendRequest(HTTPMethod.GET, "");
-		assertEquals("Web resource not found!", response);
+		WSStuff simpleConnect = new WSStuff("http://devcloud.fulgentcorp.com/doesnotexit/test.php");
+		try{
+			simpleConnect.sendRequest(HTTPMethod.GET, "");
+		}catch(RuntimeException e){
+			assertEquals("Web resource not found!", e.getMessage());
+		}
+		
 	}
 	
+	
+	/*
+	 * Test sendRequest
+	 */
+
+	//@Test(expected = RuntimeException.class)
+	@Test(timeout = 5000)
+	public void testbadSSL(){
+		WSStuff simpleConnect = new WSStuff("http://devcloud.fulgentcorp.com/testbad.php");
+		simpleConnect.initSSLContext( new File("fulgentcorp.cer"));
+		try{
+			simpleConnect.sendRequest(HTTPMethod.GET, "");
+		}catch(Exception e){
+			assertEquals("test timed out after 5000 milliseconds", e.getMessage());
+		}
+		//assertEquals("this is only a test", response);
+	}
+	
+	
+	/*
+	 * Test initSSLContext
+	 */
+
 	@Test
 	public void testSSL(){
 		WSStuff simpleConnect = new WSStuff("http://devcloud.fulgentcorp.com/test.php");
@@ -42,8 +88,40 @@ public class TestWebService {
 		assertEquals("this is only a test", response);
 	}
 	
+	
 	@Test
-	public void timeout_test(){
+	public void testSSL_nullcert(){
+		File file = null;
+		WSStuff simpleConnect = new WSStuff("http://devcloud.fulgentcorp.com/test.php");
+		try{
+			simpleConnect.initSSLContext( file);
+		}catch(IllegalArgumentException e){
+			assertEquals("Invalid cert file!", e.getMessage());
+		}
 		
 	}
+	
+	@Test
+	public void testSSL_notcerfile(){
+		String file = "notacert.txt";
+		
+		WSStuff simpleConnect = new WSStuff("http://devcloud.fulgentcorp.com/test.php");
+		try{
+			simpleConnect.initSSLContext( new File(file));
+		}catch(RuntimeException e){
+			assertEquals("Cannot create SSLContext: Cannot create SSLContext: initKeyStore returned false", e.getMessage());
+		}
+	}
+
+	
+	@Test
+	public void testHTTP1(){
+		WSStuff simpleConnect = new WSStuff("http://devcloud.fulgentcorp.com/test.php");
+		String response  = simpleConnect.sendRequest(HTTPMethod.GET, "");
+		assertEquals("this is only a test", response);
+		
+	}
+	
+
+	
 }
